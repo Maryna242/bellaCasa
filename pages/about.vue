cf3e7ea228eb54ac7eeb15435d71ea4dd59b89e6
Maryna242 <marinachapalyuk@gmail.com>
2023-07-05 19:50:00 +0300

    create project

---
+++ pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+    <castomcomponent />
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default {
+  theme: {
+    fontFamily: {
+        'fixel': ['Fixel'],
+        'body': ['"Open Sans"'],
+    }
+  }
+}
