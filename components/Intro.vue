1cbbc809ea34278a1cdf54043b89bbb4a843f632
Maryna242 <marinachapalyuk@gmail.com>
2023-07-07 17:41:08 +0300

    добавлена форма

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="max-w-[1440px] my-0 mx-auto absolute">
-        <div class="w-full flex relative">
-            <div class="flex">
+    <div class="w-full my-0  absolute">
+        <div class="max-w-[1440px] mx-auto flex relative">
+            <div class="flex w-1/2">
                 <img src="../static/img/hui.png" alt="gif" class=" max-w-full h-auto">                
             </div>
-            <div class="bg-[#0B3B60] relative before:content-[''] before:bg-mosaic before:absolute before:right-0 before:top-0 before:bottom-0 before:w-full before:max-w-[350px] before:bg-no-repeat before:bg-cover max-w-3xl w-full flex" >
+            <div class="bg-[#0B3B60] relative before:content-[''] before:bg-mosaic before:absolute before:right-0 before:top-0 before:bottom-0 before:w-full before:max-w-[350px] before:bg-no-repeat before:bg-cover w-1/2 flex" >
             </div>
             <the-form />
         </div>
--- components/TheForm.vue
+++ components/TheForm.vue
@@ -1,11 +1,35 @@
 <template>
-    <div class="flex absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div class="flex flex-col items-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
         <h1 class="text-white font-semibold text-8xl font-sourcesans">
             Bella Casa
         </h1>
-        <h2 class="text-white font-semibold font-[montserat]">
+        <h2 class="text-white font-semibold font-[montserat] text-[32px] mb-[30px]">
             Штори. Жалюзі. Карнизи. Текстильний дизайн
         </h2>
+        <form class="bg-[#D9D9D942]/20 border-[1px] border-[#DFDFE84D] rounded-xl border-solid p-6 backdrop-blur-xl">
+            <p class="text-white font-fixel font-medium text-center">
+                Безкоштовна консультація декоратора вже сьогодні!
+            </p>
+            <div class="gap-2 flex flex-wrap py-3 justify-center">
+                <input type="text" placeholder="Ваше ім’я" name="name" class="p-3 rounded">
+                <input type="tel" name="telephone" placeholder="Телефон" class="p-3 rounded">
+                <button class="bg-[#F8B1AB] py-3 px-9 rounded text-white font-fixel">
+                    Замовити дзвінок
+                </button>
+            </div>
+            <p class="flex text-white text-sm gap-1">
+                <img src="../static/img/map-pin.png" width="24px" height="24px" alt="map-pin">
+                Салони: Київ, Дніпро, Кам’янське
+            </p>
+        </form>
+        <div>
+            <p class="text-white font-sourcesans font-normal text-lg rotate-180">
+                в і д е о д и в и т и с ь
+            </p>
+            <a href="#">
+                <img src="../static/img/Polygon.png" width="25px" height="25px" alt="play"> 
+            </a>
+        </div>
     </div>
 </template>
 
