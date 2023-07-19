3a4b4853afff390b921541f6af76b0b0269adb16
Maryna242 <marinachapalyuk@gmail.com>
2023-07-19 18:56:21 +0300

    доделан блок the-history(вёрстка), сделан компонент the-employees (разметка, фото, стили)

---
+++ components/TheEmployees.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="my-[100px]">
+        <div class="grid grid-cols-3 gap-[72px]">
+            <div class="flex flex-col gap-6">
+                <h2 class=" font-[montserrat] font-semibold text-[32px] text-[#4E4747]">
+                    Найкрутіші творці затишку та стилю
+                </h2>
+                <p class=" font-fixel font-normal text-[#2B2B2B]">
+                    Наші експерти з дизайну, шиття та монтажу штор об'єднали свої сили, щоб надати вам найкращі послуги 
+                    і зробити ваш інтер'єр неповторним.
+                </p>
+            </div>
+            <ul v-for="(item, index) in ourEmployees" 
+            class=""
+            :key="index"
+            >
+                <li class="text-center font-fixel">
+                    <img :src="item.url" alt="photo" width="295px" height="340px">
+                    <div class=" mt-3 mb-1 font-medium text-lg text-[#2B2B2B]">
+                        {{ item.name }}
+                    </div>
+                    <div class=" border-b pb-3 border-[#D9D9D9] font-light text-[15px] text-[#2B2B2B]">
+                        {{ item.title }}
+                    </div>
+                    <div class="mt-2 font-light text-[#4E4747] text-[15px]">
+                        {{ item.city }}
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import Oksana from '~/static/img/Oksana.png'
+import Maryna from '~/static/img/Maryna.png'
+import Inna_Lilia from '~/static/img/Inna_Lilia.png'
+import Lyuda from '~/static/img/Lyuda.png'
+import Olga from '~/static/img/Olga.png'
+export default {
+    name: 'TheEmployees',
+    data(){
+        return{
+            ourEmployees:[
+                {
+                    url: Oksana,
+                    name:'Оксана',
+                    title: 'управляюча, координатор проектів ',
+                    city: 'м. Кам’янське',
+                },
+                {
+                    url: Maryna,
+                    name: 'Оксана',
+                    title: 'головний дизайнер декоратор ',
+                    city: 'м. Кам’янське',
+                },
+                {
+                    url: Inna_Lilia,
+                    name: 'Інна та Лілія',
+                    title: 'дизайнери декоратори ',
+                    city: 'м. Київ',
+                },
+                {
+                    url: Lyuda,
+                    name: 'Людмила',
+                    title: 'головний технолог ',
+                    city: 'м. Київ',
+                },
+                {
+                    url: Olga,
+                    name: 'Ольга',
+                    title: 'головний дизайнер декоратор ',
+                    city: 'м. Дніпро',
+                },
+            ]
+        }
+    }
+}
+</script>
--- components/TheHistory.vue
+++ components/TheHistory.vue
@@ -1,24 +1,55 @@
 <template>
     <div class="w-full max-w-[1440px] my-0 mx-auto">
         <div class="max-w-[1240px] my-0 mx-auto px-[105px]">
-            <div class="">
-                <div class="flex">
-                    <img src="../static/img/photo_2023-06-07_19-31-49.png" alt="photo" width="400px" height="552px" class=" relative">
-                    <a href="#" class="play flex w-[115px] h-[115px] items-center border border-[#F8B1AB] bg-[#F8B1AB] rounded-full justify-center absolute right-0">
+            <div class="flex gap-[128px]">
+                <div class="flex relative">
+                    <img src="../static/img/photo_2023-06-07_19-31-49.png" alt="photo" width="400px" height="552px" class=" object-cover max-h-[552px]">
+                    <a href="#" class="play flex w-[115px] h-[115px] items-center border border-[#F8B1AB] bg-[#F8B1AB] rounded-full justify-center absolute">
 
                     </a>
                 </div>
-                <div class="">
-
+                <div class="max-w-[500px]">
+                    <h2 class=" font-[montserrat] font-semibold text-[32px] text-[#4E4747] mb-6">
+                        Історія нашого бренду, або про гарантію
+                    </h2>
+                    <div class="flex gap-4 flex-col font-fixel font-light text-[#2B2B2B] text-[15px]">
+                        <p class="">
+                            Салон штор Bella Casa відзначається неперевершеним стилем і елегантністю. 
+                            Завдяки нашому досвіду і компетентності, ми забезпечуємо професійну консультацію з 
+                            вибору тканини, розробку індивідуального дизайну та налаштування шторного обладнання.
+                        </p>
+                        <p class="">
+                            Ви просто телефонуєте нам, ділитеся історією і ми надаємо комплексний сервіс 
+                            від початкової консультації до встановлення штор. Ми беремо на себе всі етапи процесу, 
+                            щоб ви могли насолоджуватися своїми новими шторами без жодних клопотів.
+                        </p>
+                        <p class="">
+                            Ми використовуємо передові технології в галузі виготовлення та управління шторами, 
+                            пропонуємо автоматизовані системи керування, які дозволяють вам зручно регулювати 
+                            освітлення та приватність у вашому приміщенні. В цьому ми найкращі!
+                        </p>
+                        <p class="">
+                            Звертаючись до нашого салону  ви можете бути впевнені, що отримаєте найвищу 
+                            якість обслуговування, дизайну та виготовлення штор, я особисто це проконтролюю.
+                        </p>
+                        <p class="text-end font-medium text-lg ">
+                            Вікторія Андерс, засновниця бренду Bella Casa.
+                        </p>
+                    </div>
                 </div>
             </div>
+            <the-employees/>
         </div>
     </div>
 </template>
 
+
 <script>
+
+import TheEmployees from './TheEmployees.vue'
 export default {
-    name: 'TheHistory'
+    name: 'TheHistory',
+    components: { TheEmployees },
 }
 </script>
 <style lang="scss">
